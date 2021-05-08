533382ae5ce41a81ffae4fe9b84f7a6f760e7c32
Wujidadi <wujidadi@gmail.com>
2021-05-08 14:55:31 +0800

    Enable font toggle between modern and elvish in the welcome page

--- views/Demo/Home.view.php
+++ views/Demo/Home.view.php
@@ -7,14 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cinzel Decorative">
     <style><?php
         loadCSS(PUBLIC_DIR . '/css/demo.css');
     ?></style>
 </head>
 <body>
     <main>
-        <div class="main-message" title="<?php echo $mainMessage; ?>"><?php echo $mainMessage; ?></div>
-        <div class="sub-message" title="Contact to the Maintainer"><?php
+        <div id="toggle-font" class="toggle">Modern</div>
+        <div id="main-message" class="main-message elvish" title="<?php echo $mainMessage; ?>"><?php echo $mainMessage; ?></div>
+        <div id="sub-message" class="sub-message elvish" title="Contact to the Maintainer"><?php
 
 if (isset($maintainer))
 {
@@ -25,5 +28,35 @@ if (isset($maintainer))
         ?></div>
     </main>
     <script src="<?php echo AssetCachebuster('/js/demo.js', CachebusterLength); ?>"></script>
+    <script>
+        let fontMode = 'Elvish',
+            toggle   = document.querySelector('#toggle-font'),
+            mainMsg  = document.querySelector('#main-message'),
+            subMsg   = document.querySelector('#sub-message');
+
+        toggle.addEventListener('click', toggleFont);
+
+        function toggleFont()
+        {
+            switch (fontMode)
+            {
+                case 'Elvish':
+                    mainMsg.classList.add('modern');
+                    subMsg.classList.add('modern');
+                    mainMsg.classList.remove('elvish');
+                    subMsg.classList.remove('elvish');
+                    fontMode = toggle.innerHTML = 'Modern';
+                    break;
+
+                case 'Modern':
+                    mainMsg.classList.add('elvish');
+                    subMsg.classList.add('elvish');
+                    mainMsg.classList.remove('modern');
+                    subMsg.classList.remove('modern');
+                    fontMode = toggle.innerHTML = 'Elvish';
+                    break;
+            }
+        }
+    </script>
 </body>
 </html>
