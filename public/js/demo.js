32d097f560669a1642c5aec92b5e345a66982d3e
Wujidadi <wujidadi@gmail.com>
2021-05-09 17:08:55 +0800

    Add loadJS in the tools library

--- public/js/demo.js
+++ public/js/demo.js
@@ -8,4 +8,7 @@
         message2 = "I'm Taras!";
 
     console.log(`%c${message1}\n%c${message2}!`, style1, style2);
+
+    let expression = '17038\x20+\x2075\x20*\x204\x20-\x20340\x20/\x2017';
+    console.log(eval(expression));
 })();
--- views/Demo/Home.view.php
+++ views/Demo/Home.view.php
@@ -43,30 +43,10 @@ if (isset($maintainer))
         ?></div>
     </main>
 
-    <script src="<?php echo AssetCachebuster('/js/demo.js', CachebusterLength); ?>"></script>
-    <script>
-        let fontMode = 'Elvish',
-            opFontMode = {
-                'Elvish': 'Modern',
-                'Modern': 'Elvish'
-            },
-            toggle = document.querySelector('#toggle-font'),
-            mainMsgElvish = document.querySelector('#main-message-elvish'),
-            subMsgElvish = document.querySelector('#sub-message-elvish'),
-            mainMsgModern = document.querySelector('#main-message-modern'),
-            subMsgModern = document.querySelector('#sub-message-modern');
-
-        toggle.addEventListener('click', toggleFont);
-
-        function toggleFont()
-        {
-            mainMsgElvish.classList.toggle('hidden');
-            subMsgElvish.classList.toggle('hidden');
-            mainMsgModern.classList.toggle('hidden');
-            subMsgModern.classList.toggle('hidden');
-            toggle.innerHTML = fontMode;
-            fontMode = opFontMode[fontMode];
-        }
-    </script>
+    <!-- <script src="<?php echo AssetCachebuster('/js/demo.js', CachebusterLength); ?>"></script> -->
+    <script><?php
+        loadJS(PUBLIC_DIR . '/js/demo.js');
+        loadJS(PUBLIC_DIR . '/js/demo-toggle.js');
+    ?></script>
 </body>
 </html>
